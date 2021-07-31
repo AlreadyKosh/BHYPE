4ae296d929013564cd8565e42fc942d7e0a03d13
AlreadyKosh <lenusui123@gmail.com>
2021-07-31 00:09:04 -0300

    31/07/2021 - Filtros Toggle Visibility

---
+++ assets/js/togglefiltro.js
@@ -0,0 +1,94 @@
+/*FUNÇÃO RECOLHER E ANIMAÇÃO DO FILTRO TAMANHO*/
+var menosTamanho = document.querySelector('.menos-toggle-tamanho');
+var tamanhos = document.querySelector('.tamanhos');
+
+menosTamanho.addEventListener('click', function(){
+
+    if(tamanhos.style.display === 'none'){
+        tamanhos.style.display = 'block';
+        
+    }
+    else
+    {
+        tamanhos.style.display = 'none';
+    }
+});
+
+const tamanho = document.querySelector('.tamanho');
+let tamanhoOpen = false;
+
+tamanho.addEventListener('click', () =>{
+    if(!tamanhoOpen){
+        tamanho.classList.add('open');
+        tamanhoOpen = true;
+    }
+    else
+    {
+        tamanho.classList.remove('open');
+        tamanhoOpen = false;
+    }
+})
+
+
+
+/*FUNÇÃO RECOLHER E ANIMAÇÃO DO FILTRO PREÇO*/
+var menosMarcas = document.querySelector('.menos-toggle-marca');
+var marcas = document.querySelector('.marcas');
+
+menosMarcas.addEventListener('click', function(){
+
+    if(marcas.style.display === 'none'){
+        marcas.style.display = 'block';
+        
+    }
+    else
+    {
+        marcas.style.display = 'none';
+    }
+});
+
+const marca = document.querySelector('.marca');
+let marcaOpen = false;
+
+marca.addEventListener('click', () =>{
+    if(!marcaOpen){
+        marca.classList.add('open');
+        marcaOpen = true;
+    }
+    else
+    {
+        marca.classList.remove('open');
+        marcaOpen = false;
+    }
+})
+
+
+/*FUNÇÃO RECOLHER E ANIMAÇÃO DO FILTRO PREÇO*/
+const preco = document.querySelector('.preco');
+let precoOpen = false;
+
+preco.addEventListener('click', () =>{
+    if(!precoOpen){
+        preco.classList.add('open');
+        precoOpen = true;
+    }
+    else
+    {
+        preco.classList.remove('open');
+        precoOpen = false;
+    }
+})
+
+var menosPreco = document.querySelector('.menos-toggle');
+var tablePreco = document.querySelector('.precos');
+
+menosPreco.addEventListener('click', function(){
+
+    if(tablePreco.style.display === 'none'){
+        tablePreco.style.display = 'block';
+    }
+    else
+    {
+        tablePreco.style.display = 'none';
+    }
+});
